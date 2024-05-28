388150278f678e6b01a805598ea85e0b8f1a0bfc
lgzhoumingjie <zhoumingjie@lvguang.ai>
2024-05-28 14:21:53 +0800

    fix: 修改主题

--- src/context/ThemeModeProvider/index.tsx
+++ src/context/ThemeModeProvider/index.tsx
@@ -1,20 +1,36 @@
-import React, { createContext, useContext, useState, FC } from 'react'
+import React, { createContext, useContext, useState, FC, useLayoutEffect } from 'react'
 import { ConfigProvider, theme } from 'antd'
-
+import './index.scss'
 interface PropsParams {
-	setThemeStatus: (s: boolean) => void
+	onSetTheme: (s: string) => void
 }
 
 const ThemeContext = createContext({} as PropsParams)
 const ThemeModeProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
 	const [themeStatus, setThemeStatus] = useState<boolean>(true) // theme: false 暗黑模式
+	useLayoutEffect(() => {
+		// 当然也可以根据本地缓存来修改默认值 如
+		if (localStorage.getItem('themeMode') === 'dark') {
+			onSetTheme('dark')
+		} else {
+			onSetTheme('light')
+		}
+	}, [])
+	const onSetTheme = (val: string) => {
+		document.documentElement.setAttribute('data-theme', val)
+		localStorage.setItem('themeMode', val)
+		setThemeStatus(val === 'light' ? true : false)
+	}
 	return (
-		<ThemeContext.Provider value={{ setThemeStatus }}>
+		<ThemeContext.Provider value={{ onSetTheme }}>
 			<ConfigProvider
 				theme={{
-					algorithm: themeStatus === false ? theme.defaultAlgorithm : theme.darkAlgorithm
+					token: {
+						colorPrimary: '#c280ff'
+					},
+					algorithm: themeStatus ? theme.defaultAlgorithm : theme.darkAlgorithm
 				}}>
-				<div className={themeStatus ? 'light' : 'dark'}>{children}</div>
+				{children}
 			</ConfigProvider>
 		</ThemeContext.Provider>
 	)
